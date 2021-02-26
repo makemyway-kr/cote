19fd5428e54ecba7fb8f90de07708f71e688f13a
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-02-26 11:49:41 +0900

    wrong in one test case
    programmers::joystick

--- joystick.cpp
+++ joystick.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 int solution(string name) {
     int answer = 0;
+    int modify_1=name.length();
+    int modify_2=name.length();
+    for(int i=0;i<name.length();i++)//수정해야하는 칸 갯수 구해두기
+    {
+        if(name[i]=='A')
+        {
+            modify_1-=1;
+            modify_2-=1;
+        }
+    }
     for(int i=0;i<name.length();i++)
     {
-        
         if(name[i]!='A')
         {
             if(name[i]-'A'>13)
@@ -18,11 +27,13 @@ int solution(string name) {
             {
                 answer+=(name[i]-'A');
             }
+            modify_1-=1;
         }
-        if(!((i==name.length()-1)||(i==name.length()-2 && name[i+1]=='A')))answer+=1;
+        if(!((i==name.length()-1)||(modify_1==0)))answer+=1;//이름을 다 고쳤거나 남은것이 모두 A이면 키를 조작하지 않음.
+        if(modify_1==0)break;//더이상 수정할 글자가 없으면 끝.
         
     }
-    int t=0;
+    int t=0;//첫 위치에서 뒤로 돌아서 이름을 수정할 때의 경우
     if(name[0]!='A')
     {
         if(name[0]-'A'>13)
@@ -30,9 +41,10 @@ int solution(string name) {
             t+=(1+('Z'-name[0]));
         }
         else
-            {
-                t+=name[0]-'A';
-            }
+        {
+            t+=name[0]-'A';
+        }
+        modify_2-=1;
     }
     t+=1;
     for(int i=name.length()-1;i>0;i--)
@@ -47,8 +59,10 @@ int solution(string name) {
             {
                 t+=name[i]-'A';
             }
+            modify_2-=1;
         }
-        if(!((i==1 )|| (i==2 && name[i-1]=='A')))t+=1;
+        if(!((i==1 )|| modify_2==0))t+=1;
+        if(modify_2==0)break;
     }
     if(t<answer)answer=t;
     
