c3c9d084687844c0301c944b0ab902b337ba08e9
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-02-27 18:42:54 +0900

    Update dev.cpp

--- dev.cpp
+++ dev.cpp
@@ -6,16 +6,16 @@ using namespace std;
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     vector<int> time;//완성 시간 vector
-    for(int i=0;i<progresses.size();i++)
+    for(int i=0;i<progresses.size();i++)//완성까지의 시간 계산
     {
         if((100-progresses[i])%speeds[i]==0)//나누어 떨어지는 경우
         {
-            time.pushback((100-progresses[i])/speeds[i]);//시간 저장
+            time.push_back((100-progresses[i])/speeds[i]);//시간 저장
 
         }
         else//나누어 떨어지지 않는 경우
         {
-            time.pushback(((100-progresses[i])/spees[i])+1);
+            time.push_back(((100-progresses[i])/speeds[i])+1);
         }
     }
     while(!time.empty())
