125a1a96a274fabc44035b9729a1ae1c94372afb
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-03-01 23:40:18 +0900

    Update n_euro_pyohyun.cpp

--- describing with N/n_euro_pyohyun.cpp
+++ describing with N/n_euro_pyohyun.cpp
@@ -1,36 +1,37 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <set>
 using namespace std;
 
 int solution(int N, int number) {
     int answer = -1;
     int temp=0;
-    vector<vector<int>>numbers;//basic numbers
-    for(int i=0;i<8;i++)
+    vector<set<int>>numbers(9);//basic numbers
+    for(int i=1;i<9;i++)
     {
-        temp=temp*10+N;
-        numbers[i].push_back(temp);
+        temp=(temp*10)+N;
+        numbers[i].insert(temp);
     }
-    for(int i=0;i<8;i++)
+    for(int i=2;i<9;i++)
     {
-        for(int u=8-i-2;u>=0;u--)
+        for(int j=1;j<9;j++)
         {
-            for(int k=0;k<numbers[i].size();k++)
+            int k=i-j;
+            for(int m:numbers[j])
             {
-                for(int j=0;j<numbers[u].size();j++)
+                for(int n:numbers[k])
                 {
-                    numbers[i+u+1].push_back(numbers[i][k]+numbers[u][j]);
-                    numbers[i+u+1].push_back(numbers[i][k]-numbers[u][j]);
-                    numbers[i+u+1].push_back(numbers[i][k]*numbers[u][j]);
-                    if(numbers[i][k]/numbers[u][j]>=1)numbers[i+u+1].push_back(numbers[i][k]/numbers[u][j]);
+                    
                 }
             }
         }
     }
-    for(int i=0;i<8;i++)
-    {
-        if(find(numbers[i].begin(),numbers[i].end(),number)!=numbers[i].end())answer=i;
-    }
+   
     return answer;
+}
+int main(void)
+{
+    cout<<solution(5,12);
 }
