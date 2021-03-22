b1140c29e0db2ae2fbd303a6da89d5d1298e1cf7
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-03-22 15:12:13 +0900

    Update bignum.py

--- bignum.py
+++ bignum.py
@@ -1,17 +1,33 @@
+def comparing(a, b):
+    a=str(a)
+    b=str(b)
+    if a+b>=b+a:
+        return True
+    else:
+        return False
 def solution(numbers):
     
     answer = ''
-    while len(numbers)!=0:
-        temp=10
-        cur=0
-        rnum=0
-        for num in numbers:
-            cur=num%10
-            if cur<temp:
-                temp=cur
-                rnum=num
-        answer=str(rnum)+answer
-        numbers.remove(rnum)
+   
+    for i in range(len(numbers)-1):
+        j=i+1
+        for j in range(len(numbers)):
+            if comparing(i,numbers[j])==False:
+                temp=0
+                temp=numbers[j]
+                numbers[j]=numbers[i]
+                numbers[i]=temp
+    if(numbers[0]=='0'):
+        return 0
 
-        
+    numbers=map(str,numbers)
+
+    for i in numbers:
+        answer=answer+i
     return answer
+   
+        
+  
+   
+        
+    
