3aad6228b57ec3f2b3889daaea6aefb39e02112c
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-03-29 00:14:52 +0900

    search_prime

--- search_primenumb/search_prime.py
+++ search_primenumb/search_prime.py
@@ -1,3 +1,21 @@
+def check_if_prime(num):
+    if(num<2):
+        return False
+    else:'''어떤 수가 a라는 수의 배수, 즉 a가 어떤 수 X의 약수이면 x/a도 X의 약수이고, 이 말은
+    x가 소수가 아니라는 것이므로 굳이 x전의 모든 수를 검사하지않고 제곱근까지만 검사하면 됨'''
+    
+
+
 def solution(numbers):
-    answer = 0
+    length=len(numbers)
+    even={}
+    odd={}
+
+    for i in range (length): '''dividing odd numbers and even numbers b/c 
+    even numbers cannot be the last digit of prime number'''
+        if int(numbers[i])%2==0:
+            even.append(numbers[i])
+        else:
+            odd.append(numbers[i])
+    
     return answer
