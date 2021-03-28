20913eedde8efea42e691607bd5a62d074b0a1b3
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-03-29 00:47:38 +0900

    Update search_prime.py

--- search_primenumb/search_prime.py
+++ search_primenumb/search_prime.py
@@ -1,11 +1,16 @@
+import math;
+from itertools import permutations
 def check_if_prime(num):
+    ret=True
     if(num<2):
         return False
     else:'''어떤 수가 a라는 수의 배수, 즉 a가 어떤 수 X의 약수이면 x/a도 X의 약수이고, 이 말은
     x가 소수가 아니라는 것이므로 굳이 x전의 모든 수를 검사하지않고 제곱근까지만 검사하면 됨'''
-    
-
-
+    for i in range(math.sqrt(num)):
+        if(num%i)==0:
+            ret=False
+            break
+    return ret
 def solution(numbers):
     length=len(numbers)
     even={}
@@ -17,5 +22,7 @@ def solution(numbers):
             even.append(numbers[i])
         else:
             odd.append(numbers[i])
+        for i in range (1,len(numbers)+1):
+            
     
     return answer
