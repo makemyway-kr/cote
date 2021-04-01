6dce48df6d61ef3f56c2e4b4e56c3dd4ce409d90
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-04-01 16:04:59 +0900

    finished

--- search_primenumb/search_prime.py
+++ search_primenumb/search_prime.py
@@ -4,21 +4,22 @@ def check_if_prime(num):
     ret=True
     if(num<2):
         return False
-    else:'''어떤 수가 a라는 수의 배수, 즉 a가 어떤 수 X의 약수이면 x/a도 X의 약수이고, 이 말은
+    else:
+        for i in range(2,int(math.sqrt(num)+1)):
+            if(num%i)==0:
+                ret=False
+                break
+        return ret
+    '''어떤 수가 a라는 수의 배수, 즉 a가 어떤 수 X의 약수이면 x/a도 X의 약수이고, 이 말은
     x가 소수가 아니라는 것이므로 굳이 x전의 모든 수를 검사하지않고 제곱근까지만 검사하면 됨'''
-    for i in range(math.sqrt(num)):
-        if(num%i)==0:
-            ret=False
-            break
-    return ret
 def solution(numbers):
     alist=[]
     length=len(numbers)
-    for i in range(1,len(numbers)+1):
-        array=list(permutations(numbers,i))
-        for n in array:
-            n=int(''.join(map(str,n)))
-            if check_if_prime(n)==True:
-                alist.append(n)
-    answerlist=list(set(alist))
-    return len(answerlist)
+    for i in range(1,length+1):
+        '''조합 갯수 반복문'''
+        perofnumbers=list(map(''.join,permutations(numbers,i)))
+        for k in perofnumbers:
+            if check_if_prime(int(k))==True:
+                alist.append(int(k))
+    anslist=list(set(alist))
+    return len(anslist)
