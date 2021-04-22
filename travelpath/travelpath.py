7943748ea679a191872aa0f957547aa5009434e0
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-04-22 13:03:43 +0900

    Update travelpath.py

--- travelpath/travelpath.py
+++ travelpath/travelpath.py
@@ -1,27 +1,22 @@
-def nextairport(tickets,depart):
+import heapq
+used_ticket=[]
+def nextairport(tickets,depart):#dfs
     nominee=[]#갈수있는 공항들 배열
     for i in range(len(tickets)):
         if tickets[i][0]==depart:
-            nominee.append([i,tickets[i][1]])
-    if len(nominee)>1:
-        temp="ZZZ"
-        tempin="10005"
-        for i in range(len(nominee)):
-            if nominee[i][1]<temp:
-                temp=nominee[i][1]
-                tempin=i
-        del tickets[nominee[tempin][0]]
-        return nominee[tempin][1]
-    elif len(nominee)==1:
-        del tickets[nominee[0][0]]
-        return nominee[0][1]
+            heapq.heappush(nominee,(tickets[i][1],i))
+    for i in nominee:
+        if (nextairport(tickets,i[0])):
+            return i[0]
 def solution(tickets):
     answer = []
     dep=nextairport(tickets,"ICN")
     answer.append("ICN")
     answer.append(dep)
-    while len(tickets)!=0: #티켓을 모두 사용할떄까지
+    count=2
+    while count=len(tickets)+1: #티켓을 모두 사용할떄까지
         nd=nextairport(tickets,dep)
         answer.append(nd)
         dep=nd
+        count+=1
     return answer
