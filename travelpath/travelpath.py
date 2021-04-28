6d690ea1adf00c5355f0cb32d66780a239a22d36
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-04-28 20:05:43 +0900

    travel_path question finished solving

--- travelpath/travelpath.py
+++ travelpath/travelpath.py
@@ -1,27 +1,22 @@
-def nextairport(tickets,depart):
-    nominee=[]#갈수있는 공항들 배열
+used_ticket=[]
+def next(tickets,curr,used):
+    count=0
     for i in range(len(tickets)):
-        if tickets[i][0]==depart:
-            nominee.append([i,tickets[i][1]])
-    if len(nominee)>1:
-        temp="ZZZ"
-        tempin="10005"
-        for i in range(len(nominee)):
-            if nominee[i][1]<temp:
-                temp=nominee[i][1]
-                tempin=i
-        del tickets[nominee[tempin][0]]
-        return nominee[tempin][1]
-    elif len(nominee)==1:
-        del tickets[nominee[0][0]]
-        return nominee[0][1]
+        if tickets[i][0]==curr and i not in used:
+            count+=1
+            used.append(i)
+            if next(tickets,tickets[i][1],used)==-1 and len(used_ticket)<len(tickets)-1:#티켓을 다 쓰지 못했는데 다음 공항이 안나오는 경우 back
+                used.remove(i)#티켓을 사용하지 않은걸로!
+                count-=1
+    if count==0:
+        return -1
 def solution(tickets):
-    answer = []
-    dep=nextairport(tickets,"ICN")
-    answer.append("ICN")
-    answer.append(dep)
-    while len(tickets)!=0: #티켓을 모두 사용할떄까지
-        nd=nextairport(tickets,dep)
-        answer.append(nd)
-        dep=nd
+    tickets.sort(key=lambda tickets :tickets[1])#도착지 이름 알파벳 순서대로 정렬
+    print(tickets)
+    global used_ticket
+    answer=["ICN"]
+    next(tickets,"ICN",used_ticket)
+    for i in used_ticket:
+        answer.append(tickets[i][1])
     return answer
+print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
