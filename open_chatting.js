cdd875f8044cc2eb1b7ec9e93568492bb8f123ec
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-05-09 17:06:38 +0900

    프로그래머스 오픈채팅방 해결

--- open_chatting.js
+++ open_chatting.js
@@ -1,23 +1,21 @@
 function solution(record) {
     var listoflog = new Array(record.length);
     for (var i = 0; i < record.length; i++) {
-        listoflog[i] = record[i].split(' ');
+        listoflog[i] = record[i].split(" ");
     }
     var iddict = {};
-    for (var i = 0; i < record.length; i++) {
-        if (listoflog[i][0] == "Enter") {
+    for (var i = 0; i < listoflog.length; i++) {
+        if (listoflog[i].length === 3) {
             iddict[listoflog[i][1]] = listoflog[i][2];
-        } else if (listoflog[i][0] == "Change") {
-            iddict[listoflog[1][1]] = listoflog[i][2];
         }
     }
     var answer = [];
-    for (var i = 0; i < record.length; i++) {
-        if (listoflog[i][0] == "Enter") {
-            var tempstring = iddict[listoflog[i][1]] + "님이" + " 들어왔습니다.";
+    for (var i = 0; i < listoflog.length; i++) {
+        if (listoflog[i][0] === "Enter") {
+            var tempstring = iddict[listoflog[i][1]] + "님이 들어왔습니다.";
             answer.push(tempstring);
-        } else if (listoflog[i][0] == "Leave") {
-            var tempstring = iddict[listoflog[i][1]] + "님이 " + "나갔습니다.";
+        } else if (listoflog[i][0] === "Leave") {
+            var tempstring = iddict[listoflog[i][1]] + "님이 나갔습니다.";
             answer.push(tempstring);
         }
     }
