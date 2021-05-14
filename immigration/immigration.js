a9cec79fe0eced7d4ffa2b92d8ed79e6d4c02d24
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-05-15 01:09:21 +0900

    Update immigration.js

--- immigration/immigration.js
+++ immigration/immigration.js
@@ -1,23 +1,36 @@
 //프로그래머스 입국심사
 function solution(n, times) {
-    var time = 0;
-    var people = n;
-    var officers = new Array(times.length);
-    for (var i = 0; i < times.length; i++) {
-        officers[i] = false; //업무중이 아니면 false
-    }
-    while (people > -times.length) {
-        for (var i = 0; i < times.length; i++) {
-            if (officers[i] == false) {
-                people -= 1;
-                officers[i] = true;
-            } else if (officers[i] == true && time % times[i] == 0) { //현재 시간을 소요시간으로 나누어 떨어지면 일이 끝남. 새 사람 받음.
-                people -= 1;
+    var ltime = 0;
+    ltime = 1; //왼쪽끝
+    var sumt = [];
+    var rtime = Math.max(...times) * n; //오른쪽
+    var time = rtime; //계산 대상 시간
+    while (ltime <= rtime) {
+        if (n == times.length) {
+            sumt.push(Math.max(...times));
+            ltime = rtime + 1;
+            break;
+        } else {
+            var temp = new Array(times.length);
+            for (var i = 0; i < times.length; i++) {
+                temp[i] = Math.floor(time / times[i]);
+            }
+            var ssum = temp.reduce(function add(tsum, currvalue) {
+                return tsum + currvalue;
+            }, 0);
+            if (ssum >= n) {
+                if (ssum == n) {
+                    sumt.push(time);
+                }
+                rtime = time - 1;
+                time = Math.floor((ltime + rtime) / 2);
+            } else if (ssum < n) {
+                ltime = time + 1;
+                time = Math.floor((ltime + rtime) / 2);
             }
 
         }
-        time++;
+
     }
-    time -= 1; //제일 마지막에도 시간이 증가하므로 하나 빼줘야함.
-    return time;
+    return Math.min(...sumt);
 }
