0ea2cc47c325872e1b4d9109fc051618357c77db
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-05-18 13:51:21 +0900

    N으로표현 에러 해결

--- describing with N/n_euro_pyohyun.cpp
+++ describing with N/n_euro_pyohyun.cpp
@@ -8,13 +8,16 @@ using namespace std;
 int solution(int N, int number) {
     int answer = -1;
     int temp=0;
-    vector<set<int>>numbers(9);//basic numbers
+    vector< set<int> >numbers(9);//basic numbers
     for(int i=1;i<9;i++)
     {
         temp=(temp*10)+N;
         numbers[i].insert(temp);
     }
-    for(int i=2;i<9;i++)
+    if(number==N)answer=1;
+    if(answer==-1)
+    {
+        for(int i=2;i<9;i++)
     {
         for(int j=1;j<i;j++)
         {
@@ -24,18 +27,21 @@ int solution(int N, int number) {
                 for(int n:numbers[k])
                 {
                     numbers[i].insert(m+n);
-                    if(m-n>0)numbers[i].insert(m-n);
+                    if(m-n>=0)numbers[i].insert(m-n);
                     numbers[i].insert(m*n);
-                    if(m/n>=1)numbers[i].insert(m/n);
+                    if(m!=0 && n!=0 && m/n>=1)numbers[i].insert(m/n);
                 }
             }
         }
-        if(numbers[i].find(number)!=numbers[i].end())return i;
+        if(numbers[i].find(number)!=numbers[i].end())
+        {
+            answer=i;
+            break;
+        }
     }
+    
+    }
+    
    
     return answer;
-}
-int main(void)
-{
-    cout<<solution(5,12);
 }
