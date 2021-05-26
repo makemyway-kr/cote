38d5722cb61b7b0cdf77259f890d7a6ef8d5239f
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-05-26 11:13:25 +0900

    Update gem_shop.py

--- gem_shop/gem_shop.py
+++ gem_shop/gem_shop.py
@@ -1,24 +1,14 @@
-import copy
 def solution(gems):
     answer = []
-    tempan=1000001
-    gemsl=[]#보석 종류 저장 list
-    for i in gems:
-        if i not in gemsl:
-            gemsl.append(i)
+    gemsl=set(gems)#보석종류 저장
     for ran in range(len(gemsl),len(gems)+1):#최소한 보석의 종류만큼은 사야함, 최대 진열장 모두
-        for s in range(len(gemsl)):#시작 위치
-            tempg=copy.deepcopy(gemsl)
-            lister=copy.deepcopy(gems[s:s+ran])
-            rlist=[]
-            for k in lister:
-                if k in tempg:
-                    tempg.remove(k)
-                    rlist.append(k)
-            if len(tempg)==0 and ran<tempan:
-                tempan=ran
-                answer.clear()
+        for s in range(len(gems)-ran+1):#시작 위치
+            temp=set(gems[s:s+ran])
+            if len(temp)==len(gemsl):
                 answer.append(s+1)
                 answer.append(s+ran)
+                break 
+        if len(answer)!=0:
+            break              
     return answer
-print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+print(solution(["DIA", "EM", "EM", "RUB", "DIA"]))
