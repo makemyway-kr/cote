1772ed5b6867fa8a16df2b86f62ee38e3ab039cd
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-07-01 15:47:15 +0900

    Update gemshop2.py

--- gem_shop/gemshop2.py
+++ gem_shop/gemshop2.py
@@ -1,15 +1,21 @@
 def solution(gems):
     answer=[]
     gemlist=set(gems)
-    key=0
-    for length in range(len(gemlist),len(gems)+1):
-        for start in range(0,len(gems)+1):
+    pool=[]
+    direct_key=0
+    for start in range(0,len(gems)-len(gemlist)):
+        for length in range(len(gemlist),len(gems)+1):
             if len(set(gems[start:start+length]))==len(gemlist):
-                answer.append(start+1)
-                answer.append(start+length)
-                key=1
-                break
-        if key==1:
+                pool.append([start+1,start+length,length])
+                if length==len(gemlist):
+                    answer.append(start+1)
+                    answer.append(start+length)
+                    direct_key=1
+                    break
+        if direct_key==1:
             break
+    if direct_key!=1:
+        pool.sort(key=lambda pool:pool[2])
+        answer=[pool[0][0],pool[0][1]]
     return answer
 solution(["XYZ", "XYZ", "XYZ"])
