395b289628b436a70be4e56dc7bc4cfd4f7006a1
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-07-04 21:05:03 +0900

    Update n_.py

--- describing with N/n_.py
+++ describing with N/n_.py
@@ -13,16 +13,16 @@ def solution(N, number):
             if number in list:
                 answer=numbers.index(list)+1
         else:
-            for usedN in range(2,9):
+            for usedN in range(2,9):#쓰일 N숫자의 수를 통제함.
                 for i in range(1,usedN):
-                    l=usedN-i
+                    l=usedN-i#N의 횟수 합이 usedN과 일치하도록.
                     for left in numbers[i-1]:
                         for right in numbers[l-1]:
                             if left+right not in numbers[usedN-1]:
                                 numbers[usedN-1].append(left+right)
                             if left*right not in numbers[usedN-1]:
                                 numbers[usedN-1].append(left*right)
-                            if right>0 and left//right>1 and left//right not in numbers[usedN-1]:
+                            if right>0 and left//right>=1 and left//right not in numbers[usedN-1]:
                                 numbers[usedN-1].append(left//right)
                             if left-right>0 and left-right not in numbers[usedN-1]:
                                 numbers[usedN-1].append(left-right)
