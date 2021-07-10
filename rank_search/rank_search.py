a87cd9b9e6b8ac101be90336b9fda96204d188d9
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-07-10 21:19:35 +0900

    Update rank_search.py

--- rank_search/rank_search.py
+++ rank_search/rank_search.py
@@ -1,15 +1,14 @@
 def solution(info, query):
     answer = []
     table={"cpp":[],"java":[],"python":[],"backend":[],"frontend":[],"junior":[],"senior":[],"chicken":[],"pizza":[]}
-    table_score=[]
+    table_score={}
     for c in range(len(info)):
         temp_info=info[c].split(" ")
         for t in temp_info:
             if t in table:
                 table[t].append(c)
             else:
-                table_score.append([c,t])
-    table_score.sort(key= lambda table_score:table_score[1])
+                table_score[c]=int(t)
     for k in query:
         temp=k.split(" ")
         candidates=[]
@@ -22,8 +21,7 @@ def solution(info, query):
                     for each in candidates:
                         if each not in table[cond]:
                             toeliminate.append(each)
-                else:#score
-                    
+                else:
                     for each in candidates:
                         if table_score.get(each)<int(cond):
                             toeliminate.append(each)
@@ -31,4 +29,3 @@ def solution(info, query):
                 candidates.remove(each)
         answer.append(len(candidates))
     return answer
-solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"])
