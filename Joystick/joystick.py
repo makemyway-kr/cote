09a09b1eaad01c54d900d8e70c3318654a500879
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-07-18 23:41:54 +0900

    Update joystick.py

--- Joystick/joystick.py
+++ Joystick/joystick.py
@@ -1,5 +1,25 @@
 def solution(name):
     answer = 0
+    to_change=0
     for i in name:
-        
+        if i !='A':
+            to_change+=1
+    left=0
+    right=0
+    i=len(name)-1
+    if name[0]!='A':
+        left+=1
+    while name[i]!='A':
+        if name[i]!='A':
+            left+=1
+        if i==0:
+            break
+        i=i-1
+    i=0
+    while name[i]!='A':
+        if name[i]!='A':
+            right+=1
+        if i==len(name)-1:
+            break
+        i=i+1
     return answer
