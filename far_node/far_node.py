46f885e4fd9275050499786a3937fe6a3c8e324f
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-08-02 00:57:10 +0900

    failed test
    3,4,6: fail
    7,8,9: timeout
    
    Used BFS with Queue

--- far_node/far_node.py
+++ far_node/far_node.py
@@ -2,7 +2,7 @@ def solution(n, edge):
     answer = 0
     visited=[]
     curr=1
-    depth=1
+    depth=0
     visit=1
     start_from=0
     while visit!=n:
@@ -11,28 +11,27 @@ def solution(n, edge):
             if curr in k:
                 to_insert=[]
                 if k[0]==curr:
-                    to_insert=[k[1],depth]
+                    to_insert=[k[1],depth+1]
                 elif k[1]==curr:
-                    to_insert=[k[0],depth]
+                    to_insert=[k[0],depth+1]
                 err=0
                 for j in visited:
                     if j[0]==to_insert[0]:
                         err=1
                 if err==0:
                     visited.append(to_insert)
+                    visit+=1
                 temp.append(k)
-                visit+=1
         for k in temp:
             edge.remove(k)
         if visited[start_from][0]==curr:
             curr=visited[start_from][1]
         else:
             curr=visited[start_from][0]
-        depth=visited[start_from][1]+1
+        depth=visited[start_from][1]
         start_from+=1
     for l in visited:
         if depth==l[1]:
             answer+=1
-    print(answer)
     return answer
 solution(6,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]])
