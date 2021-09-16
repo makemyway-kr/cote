de75bee379d4b458a71987cc8bc49efbbd46bd35
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-09-16 14:05:07 +0900

    Update same_name.sql

--- programmers/SQL/고양이와개는몇마리있을까/same_name.sql
+++ programmers/SQL/고양이와개는몇마리있을까/same_name.sql
@@ -1,2 +1,6 @@
 SELECT NAME,COUNT(NAME) as cntname from ANIMAL_INS GROUP BY NAME HAVING cntname>1 ORDER BY NAME;
 /*HAVING 절->GROUP BY 에서 조건을 달아줌.*/
+/*보호소에 동물이 몇마리가 들어왔을까*/
+SELECT COUNT(NAME) as cnt from ANIMAL_INS;
+/*중복제거하기 : DISTINCT를 사용하면 중복을 제거할 수 있다!!!*/
+SELECT COUNT(DISTINCT NAME)from ANIMAL_INS WHERE NAME is not NULL
