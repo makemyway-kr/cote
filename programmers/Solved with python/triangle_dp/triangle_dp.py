f4b7587ff56bfe28af9d22f468490990c288b9dc
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2021-09-21 18:44:50 +0900

    Update triangle_dp.py

--- programmers/Solved with python/triangle_dp/triangle_dp.py
+++ programmers/Solved with python/triangle_dp/triangle_dp.py
@@ -1,13 +1,18 @@
 #프로그래머스 정수삼각형 문제 https://programmers.co.kr/learn/courses/30/lessons/43105?language=python3
-finalcals=[]
-def dp(triangle,currow,currcol,curr):
-    if(currow!=len(triangle)-1):
-        dp(triangle,currow+1,currcol,curr+triangle[currow+1][currcol])
-        dp(triangle,currow+1,currcol+1,curr+triangle[currow+1][currcol+1])
-    else:
-        finalcals.append(curr)
+#삼각형을 행이 2개인것부터 하여 문제를 풀어나가는 것 처럼 푼다->dp
 def solution(triangle):
-    #각 숫자의 행번호는 +1 이고 열 번호가 같은것, 또는 1 큰것으로 이동 가능함. [1][0]이면 [2][0] 또는 [2][1]로 이동가능
-    dp(triangle,0,0,triangle[0][0])
-    return sorted(finalcals,reverse=True)[0]
+    if len(triangle)>2:
+        triangle[1][0]+=triangle[0][0]
+        triangle[1][1]+=triangle[0][0]
+        if(len(triangle)>3):
+            for row in range(2,len(triangle)):
+                for col in range(len(triangle[row])):
+                    if col==0 or col==len(triangle[row])-1:
+                        if col==0:
+                            triangle[row][col]+=triangle[row-1][col]
+                        else:
+                            triangle[row][col]+=triangle[row-1][col-1]
+                    else:
+                        triangle[row][col]+=max(triangle[row-1][col-1],triangle[row-1][col])
+    return max(triangle[len(triangle)-1])
 print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
