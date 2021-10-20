6018db61070b7e4994ae4aba4f4b2145bb4d668f
SamSim <jun78111209@gmail.com>
2021-10-20 11:19:58 +0900

    runtime error

--- programmers/Solved with python/bestalbum/bestalbum.py
+++ programmers/Solved with python/bestalbum/bestalbum.py
@@ -6,11 +6,16 @@ def solution(genres, plays):
         if genres[g] not in genre_idx:
             genre_idx.append(genres[g])
             temp=[g]
+            tempp=plays[g]
             for i in range(g+1,len(genres)):
                 if genres[i]==genres[g]:
                     temp.append(i)
-            sort_with_genre.append(temp)
-    sort_with_genre.sort(key=lambda x: len(x))
-    print(sort_with_genre)
+                    tempp+=plays[i]
+            sort_with_genre.append([temp,tempp])
+    sort_with_genre.sort(key=lambda x: x[1],reverse=True)
+    for s in sort_with_genre:
+        s[0].sort(key=lambda x: plays[x],reverse=True)
+        answer.append(s[0][0])
+        answer.append(s[0][1])
     return answer
 solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500])
