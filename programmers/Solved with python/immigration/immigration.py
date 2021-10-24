8a0f8552909243c2c752a6f228c69d53e64cffaf
SamSim <jun78111209@gmail.com>
2021-10-24 14:01:59 +0900

    정답
    입국심사를 받을 사람의 수와 심사관의 수가 같을 경우 가장 오래걸리는 심사관의 시간으로 해주었다. (다 각기 다른 심사대로 간다는 생각으로) 하지만 문제의 조건에, 심사대가 비어있더라도 대기시간+심사시간이 비어있는 심사대보다 짧다면 대기하기에 해당 풀이는 옳지 않았다.

--- programmers/Solved with python/immigration/immigration.py
+++ programmers/Solved with python/immigration/immigration.py
@@ -1,28 +1,20 @@
 def solution(n, times):
-    answer = []
+    answer=0
     times.sort(key=lambda x : x)
     right=n*times[len(times)-1]#right side is the maximum time needed when the office needing most time does all jobs
     left=1
     mid=right+left//2
-    if len(times)==n:
-        answer.append(max(times))
-    elif n==1:
-        answer.append(min(times))
-    else:
-        while(left<=right):
-            mid=(right+left)//2
-            handled=0#people who finished immigration
-            for t in times: #people will go to immigration officer who needs less time first
-                handled+=mid//t
-                if handled>=n:
-                    break #if people finished immigration exceeds n or same, finish for loop
-            if handled>n:
-                right=mid-1
-                answer.append(mid)
-            elif handled<n:
-                left=mid+1
-            else:
-                answer.append(mid)
-                right=mid-1
-    return min(answer)
-print(solution(3,[7,10]))
+    while(left<=right):
+        mid=(right+left)//2
+        handled=0#people who finished immigration
+        for t in times: #people will go to immigration officer who needs less time first
+            handled+=mid//t
+            if handled>=n:
+                break #if people finished immigration exceeds n or same, finish for loop
+        if handled>=n:
+            right=mid-1
+            answer=mid
+        elif handled<n:
+            left=mid+1
+    return answer
+#print(solution(6,[7,10]))
