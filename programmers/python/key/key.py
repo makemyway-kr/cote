c2593e2accb4dfeb963c863991126206abeb3601
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-02-11 21:27:20 +0900

    Update key.py

--- programmers/python/key/key.py
+++ programmers/python/key/key.py
@@ -1,1 +1,6 @@
 #https://programmers.co.kr/learn/courses/30/lessons/60059 level 3문제
+def solution(key,lock):
+    answer = True
+    panding = len(key-1)
+    key_matcher = [[1 for col in range (len(lock) + panding)] for row in range (len(lock) + panding)]
+    return answer
