932d14d0983c09c7a7fd11aa9b04f85f0f18deff
SamSim <jun78111209@gmail.com>
2022-05-02 18:29:32 +0900

    Update edit3.py

--- programmers/python/kakao_infra/editingtable/edit3.py
+++ programmers/python/kakao_infra/editingtable/edit3.py
@@ -1,31 +1,54 @@
-from operator import indexOf
-
 def solution(n, k, cmd):
     answer = ''
     temp = [1 for f in range (n)]
-    live = [x for x in range(n)]
+    start = 0
+    last = n-1
     curr = k
+    links = [[x-1,x+1] for x in range(n)]
     last_deleted = []
     for c in cmd:
         if c[0] == 'C':
             temp[curr] = 0
             last_deleted.append(curr)
-            if curr == live[-1]:
-                curr = live[-2]
+            if curr == last : 
+                curr = links[curr][0]
+                last = curr
+                links[curr][1]=n
+
+            elif curr == start:
+                curr = links[curr][1]
+                start = curr
+                links[curr][0] = -1
             else:
-                curr = live[indexOf(live,last_deleted[-1])+1]
-            live.pop(indexOf(live,last_deleted[-1]))
+                curr = links[curr][1]
+                links[curr][0] = links[links[curr][0]][0]
+                
         elif c[0] == 'Z':
             to_recover = last_deleted.pop()
-            live.append(to_recover)
-            live.sort()
+            if to_recover < start :
+                start = to_recover
+            elif to_recover > last:
+                last = to_recover
             temp[to_recover] = 1
         else:
             to_move = int(c.split(' ')[1])
+            count = 0
+            after = 0
             if c[0] == 'D':
-                curr = live[indexOf(live,curr)+to_move]
+                for i in range(curr+1 , last+1):
+                    if temp[i] == 1:
+                        count +=1
+                    if count == to_move:
+                        after = i
+                        break
             elif c[0] == 'U':
-                curr = live[indexOf(live,curr)-to_move]
+                for i in range(curr-1 , start-1 , -1):
+                    if temp[i] == 1:
+                        count +=1
+                    if count == to_move:
+                        after = i
+                        break
+            curr = after
     for i in temp:
         if i == 1:
             answer+="O"
