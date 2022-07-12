c7ccbdef62b26c0c8378c54b130dc53d63015dd5
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-07-12 18:15:26 +0900

    왜 안되는겨...

--- BOJ/python/interviewee.py
+++ BOJ/python/interviewee.py
@@ -4,7 +4,16 @@ import sys
 T = int(sys.stdin.readline().rstrip())
 for t in range(T):
     people = []
+    drop = []
     N = int(sys.stdin.readline().rstrip())
     for n in range(N):
         temp = [int(x) for x in sys.stdin.readline().rstrip().split(' ')]
         people.append(temp)
+    people.sort(key=lambda x: x[0])
+    for p in range(N-1):
+        if p in drop:
+            continue
+        for p2 in range(p+1, N):
+            if (p2 not in drop) and (people[p][0] > people[p2][0]) and (people[p][1] > people[p2][1]):
+                drop.append(p2)
+    print(N-len(drop))
