4a94b2e30ea72852d4c77c342ec4a74cdbff9857
SamSim <58161987+makemyway-kr@users.noreply.github.com>
2022-08-15 23:30:04 +0900

    ccw 11758

--- BOJ/python/11758.py
+++ BOJ/python/11758.py
@@ -7,24 +7,10 @@ for i in range(3):
     x.append(t1)
     y.append(t2)
 
-a1 = y[1]-y[0]
-a2 = x[1]-x[0]
-a = 0
-b = 0
-if a2 != 0:
-    a = a1/a2
-    b = y[0]-(x[0]*(a))
-    c = (x[2]*(a)) + b
-    if c == y[2]:
-        print(0)
-    elif c < y[2]:  # p1,p2를 이은 직선보다 위에 p3점이 위치하면 반시계
-        print(1)
-    else:  # p1,p2를 이은 직선보다 아래에 p3점이 위치하면 시계방향
-        print(-1)
-else:  # x=c 형태의 직선인 경우
-    if x[2] == x[1]:
-        print(0)
-    elif x[2] < x[1]:
-        print(1)
-    else:
-        print(-1)
+ccw = (x[0]*y[1]+x[1]*y[2]+x[2]*y[0])-(x[1]*y[0]+x[2]*y[1]+x[0]*y[2])
+if ccw == 0:
+    print(0)
+elif ccw > 0:
+    print(1)
+elif ccw < 0:
+    print(-1)
